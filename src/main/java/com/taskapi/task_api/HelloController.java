980ab585749ccc19c732c9d2dab3c522f65db056
George Lynch <glynch77@icloud.com>
2026-02-07 00:22:49 -0600

    Initial commit - Hello World API working

---
+++ src/main/java/com/taskapi/task_api/HelloController.java
@@ -0,0 +1,15 @@
+package com.taskapi.task_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World! My first API is working!";
+    }
+}
+
+
